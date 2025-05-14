50b64c97640346c40b3c01b9d2ed21594e3224b8
afslucass <afslucass@gmail.com>
2025-05-14 20:44:45 -0300

    feat: adjust navbar and background

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,25 +1,33 @@
 import "./styles.css";
 
-import Logo from "../../assets/logo.svg";
+import Logo from "../../assets/icons/logo.svg";
+import Menu from "../../assets/icons/menu.svg";
+import Bell from "../../assets/icons/bell.svg";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <div className="navbar-top">
-        <div className="left"></div>
-        <div className="title">
-          <img className="img" src={Logo} alt="Logo" />
+    <div className="margin">
+      <div className="navbar">
+        <div className="navbar-top">
+          <div className="left">
+            <img className="img" src={Menu} alt="menu" />
+          </div>
+          <div className="title">
+            <img className="img" src={Logo} alt="Logo" />
+          </div>
+          <div className="right">
+            <img className="img" src={Bell} alt="bell" />
+          </div>
         </div>
-        <div className="right"></div>
-      </div>
-      <div>
-        <input
-          className="input"
-          type="text"
-          placeholder="Buscar produtos"
-          name=""
-          id=""
-        />
+        <div>
+          <input
+            className="input"
+            type="text"
+            placeholder="Buscar produtos"
+            name=""
+            id=""
+          />
+        </div>
       </div>
     </div>
   );

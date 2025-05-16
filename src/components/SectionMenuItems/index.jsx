7d45b78a60a097a57c6bf91a5adcf0f5d1f2da36
afslucass <afslucass@gmail.com>
2025-05-15 21:21:26 -0300

    feat: add fonts

--- src/components/SectionMenuItems/index.jsx
+++ src/components/SectionMenuItems/index.jsx
@@ -11,39 +11,43 @@ const SectionMenuItems = ({ title }) => {
             src="/images/foods/menu-item-model-1.png"
           />
           <div className={styles.right}>
-            <div>Steak do Cowboy</div>
+            <div className={styles.item_menu_title}>Steak do Cowboy</div>
             <div className={styles.description_container}>
-              <div>42min</div>
+              <div className={styles.item_menu_text}>42min</div>
               <div className={styles.dot} />
-              <div>R$34,90</div>
+              <div className={styles.item_menu_text}>R$34,90</div>
             </div>
           </div>
         </div>
+      </div>
+      <div>
         <div className={styles.menu_item}>
           <img
             className={styles.food_image}
             src="/images/foods/menu-item-model-1.png"
           />
           <div className={styles.right}>
-            <div>Steak do Cowboy</div>
+            <div className={styles.item_menu_title}>Steak do Cowboy</div>
             <div className={styles.description_container}>
-              <div>42min</div>
+              <div className={styles.item_menu_text}>42min</div>
               <div className={styles.dot} />
-              <div>R$34,90</div>
+              <div className={styles.item_menu_text}>R$34,90</div>
             </div>
           </div>
         </div>
+      </div>
+      <div>
         <div className={styles.menu_item}>
           <img
             className={styles.food_image}
             src="/images/foods/menu-item-model-1.png"
           />
           <div className={styles.right}>
-            <div>Steak do Cowboy</div>
+            <div className={styles.item_menu_title}>Steak do Cowboy</div>
             <div className={styles.description_container}>
-              <div>42min</div>
+              <div className={styles.item_menu_text}>42min</div>
               <div className={styles.dot} />
-              <div>R$34,90</div>
+              <div className={styles.item_menu_text}>R$34,90</div>
             </div>
           </div>
         </div>

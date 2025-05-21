e0641acfb4195aecda132ae745121dd81e4db520
afslucass <afslucass@gmail.com>
2025-05-21 20:53:58 -0300

    feat: add products.json

--- src/components/HorizontalMenuItems/index.jsx
+++ src/components/HorizontalMenuItems/index.jsx
@@ -1,50 +1,22 @@
+import { formatMoney } from "../../helpers/format-money";
 import styles from "./styles.module.css";
 
-const HorizontalMenuItems = () => {
+const HorizontalMenuItems = ({ list }) => {
   return (
     <div className={styles.list}>
-      <div className={styles.item_container}>
-        <div className={styles.food_image_container}>
-          <img
-            className={styles.food_image}
-            src="/images/foods/menu-item-model-1.png"
-          />
+      {list.map((item) => (
+        <div className={styles.item_container}>
+          <div className={styles.food_image_container}>
+            <img className={styles.food_image} src={item.imageUrl} />
+          </div>
+          <div className={styles.title}>{item.name}</div>
+          <div className={styles.description_container}>
+            <div>{item.duration}min</div>
+            <div className={styles.dot} />
+            <div>R${formatMoney(item.price)}</div>
+          </div>
         </div>
-        <div className={styles.title}>Steak do Cowboy</div>
-        <div className={styles.description_container}>
-          <div>42min</div>
-          <div className={styles.dot} />
-          <div>R$34,90</div>
-        </div>
-      </div>
-      <div className={styles.item_container}>
-        <div className={styles.food_image_container}>
-          <img
-            className={styles.food_image}
-            src="/images/foods/menu-item-model-1.png"
-          />
-        </div>
-        <div className={styles.title}>Steak do Cowboy</div>
-        <div className={styles.description_container}>
-          <div>42min</div>
-          <div className={styles.dot} />
-          <div>R$34,90</div>
-        </div>
-      </div>
-      <div className={styles.item_container}>
-        <div className={styles.food_image_container}>
-          <img
-            className={styles.food_image}
-            src="/images/foods/menu-item-model-1.png"
-          />
-        </div>
-        <div className={styles.title}>Steak do Cowboy</div>
-        <div className={styles.description_container}>
-          <div>42min</div>
-          <div className={styles.dot} />
-          <div>R$34,90</div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
--- src/screens/Menu/Menu.jsx
+++ src/screens/Menu/Menu.jsx
@@ -2,15 +2,16 @@ import Navbar from "../../components/NavBar";
 import HorizontalMenuItems from "../../components/HorizontalMenuItems";
 import SectionMenuItems from "../../components/SectionMenuItems";
 import Details from "../../components/Details";
+import { PRODUCTS } from "../../constants/products";
 
 function Menu() {
   return (
     <>
       <Navbar />
-      <HorizontalMenuItems />
-      <SectionMenuItems title="Entradas" />
-      <SectionMenuItems title="Parrilla" />
-      <SectionMenuItems title="Hamburguer" />
+      <HorizontalMenuItems list={PRODUCTS.carrousel} />
+      <SectionMenuItems title="Entradas" list={PRODUCTS.open} />
+      <SectionMenuItems title="Parrilla" list={PRODUCTS.parrilla} />
+      <SectionMenuItems title="Hamburguer" list={PRODUCTS.hamburguers} />
       <Details />
     </>
   );

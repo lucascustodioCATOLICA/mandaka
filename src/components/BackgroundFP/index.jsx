d90077cf171922e7278dd69a62bc6684d8ce463a
Arthur-Faria-dos-Santos <af096396@gmail.com>
2025-06-02 19:41:03 -0300

    feat ajustes em telas

---
+++ src/components/BackgroundFP/index.jsx
@@ -0,0 +1,15 @@
+import styles from "./styles.module.css";
+
+function BackgroundFP() {
+  return (
+    <div className={styles.container}>
+      <img
+        className="backgroud"
+        src="../../../public/images/login/backgroudvector.png"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default BackgroundFP;
--- src/infra/router/AppRouter.jsx
+++ src/infra/router/AppRouter.jsx
@@ -5,7 +5,7 @@ import Carrinho from "../../screens/Carrinho/Carrinho";
 import FirstPage from "../../screens/FirstPage/FirstPage";
 import Cadastro from "../../screens/Cadastro/Cadastro";
 import Login from "../../screens/Login/Login";
-
+import NoLogin from "../../screens/NoLogin/NoLogin";
 
 const AppRouter = () => {
   return (
@@ -13,10 +13,10 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Menu />} />
         <Route path="/carrinho" element={<Carrinho />} />
-        <Route path="/first-page" element={<FirstPage/>} />
-        <Route path="/cadastro" element={<Cadastro/>} />
-        <Route path="/login" element={<Login/>} />
-
+        <Route path="/first-page" element={<FirstPage />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/no-login" element={<NoLogin />} />
       </Routes>
     </HashRouter>
   );

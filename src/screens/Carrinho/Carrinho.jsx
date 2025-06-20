b7e7f8adcd361fa1c83b5f26f2f01bd4f061ff5e
afslucass <afslucass@gmail.com>
2025-06-19 21:10:22 -0300

    fix: add logs to analyse bug

--- src/screens/Carrinho/Carrinho.jsx
+++ src/screens/Carrinho/Carrinho.jsx
@@ -44,14 +44,17 @@ function Carrinho() {
 
   const handlePlusButtonPress = (id) => {
     setProducts((prev) => {
+      console.log(prev);
       const newProducts = prev.map((item) => {
         if (item.id !== id) {
           return item;
         }
+        console.log(item);
         const newCount = item.count++;
         const newPrice = newCount * item.product.price;
         return { ...item, count: newCount, price: newPrice };
       });
+      console.log(newProducts);
       ProductsStorage.updateProducts(newProducts);
       return newProducts;
     });
